3c7099dc651ecec3c13413e32c0b5dbc82768c93
pinheiro767 <claudiapinheiro767@gmail.com>
2026-03-30 07:53:30 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+// sw.js
+const CACHE_NAME = "anatomia-clinica-v1";
+const URLS = ["./", "./index.html", "./manifest.json"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
